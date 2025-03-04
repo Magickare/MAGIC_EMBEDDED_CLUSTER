61efc3a8fd90555e1320108d27ed6d7bae75395f
MAGIC KARE EMBEDDED CLUSTER <magic.kare.embedded@gmail.com>
2025-03-04 16:36:48 +0530

    Update mpu6050.h

--- DMA_I2C_BAREMETAL/Core/Inc/mpu6050.h
+++ DMA_I2C_BAREMETAL/Core/Inc/mpu6050.h
@@ -3,19 +3,21 @@
 
 #include "stm32f4xx_hal.h"
 
-
+/* MPU6050 I2C Address */
 #define MPU6050_ADDR 0xD0
 
-
+/* MPU6050 Register Addresses */
 #define WHO_AM_I_REG  0x75
 #define PWR_MGMT_1    0x6B
 #define ACCEL_XOUT_H  0x3B
 #define GYRO_XOUT_H   0x43
 
-
+/* Function Declarations */
 void MPU6050_Init(I2C_HandleTypeDef *hi2c);
 void MPU6050_Read_Accel_Gyro_DMA(I2C_HandleTypeDef *hi2c);
 
-extern uint8_t MPU6050_Data[14];
+/* External Variables */
+extern volatile uint8_t dma_complete; // Declare as extern
+extern uint8_t mpu_data[14]; // Declare as extern
 
 #endif
